940989a12192fea3dd5228ebbcd9cfcafe83f473
aarondizele <aldizele@gmail.com>
2020-06-04 16:22:47 +0100

    Database conn

--- afrilex/settings.py
+++ afrilex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,21 +84,15 @@ WSGI_APPLICATION = 'afrilex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'AFRILEX_TEST',
-        'USER': 'postgres',
-        'PASSWORD': '$Laxis1994$',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
 '''
 DATABASES = {
     'default': {
